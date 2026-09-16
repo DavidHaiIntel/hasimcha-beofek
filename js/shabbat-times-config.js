5bd6bc406b4d8dbf626157e1d206ee074d91a230
DavidHaiIntel <david.hai.yossef.cohen@intel.com>
2026-09-16 14:39:30 +0300

    עדכון זמני תפילות שבת

--- js/shabbat-times-config.js
+++ js/shabbat-times-config.js
@@ -12,5 +12,6 @@ const SHABBAT_TIMES_CONFIG = {
     { key: "limudHorim", label: "לימוד הורים וילדים", override: "", hidden: false },
     { key: "minchaShabbat", label: "מנחה של שבת", override: "", hidden: false },
     { key: "arvitMotzash", label: "ערבית של מוצ\"ש", override: "", hidden: false },
+    { key: "", label: "שיעור של הרב דוד אסולין לשבת תשובה", override: "10:30", hidden: false },
   ],
 };
